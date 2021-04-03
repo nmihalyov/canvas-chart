3b50c355782ffa72823fdfdeb2830c847727c618
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-04-03 10:08:45 +0300

    Draw chart lines from data

--- chart.js
+++ chart.js
@@ -9,7 +9,10 @@ const DPI_SIZES = {
 };
 const GRID_LINES = 5;
 const PADDING = 40;
-const VIEW_HEIGHT = DPI_SIZES.HEIGHT - PADDING * 2;
+const VIEW = {
+  WIDTH: DPI_SIZES.WIDTH,
+  HEIGHT: DPI_SIZES.HEIGHT - PADDING * 2
+};
 const THEME = {
   GRID_LINES: {
     WIDTH: 2,
@@ -19,24 +22,34 @@ const THEME = {
     FONT: 'normal 20px Helvetica, sans-serif',
     FILL: '#96A2AA'
   },
-  LINE_PRIMARY: {
-    WIDTH: 4,
-    COLOR: '#2FBD2F'
-  }
+  LINE_WIDTH: 4
+};
+
+// Fetch data from JSON file
+const getData = async url => {
+  const res = await fetch(url);
+  return await res.json();
 };
 
 // Get min and max values
-const getMinMax = data => {
+const getMinMax = ({ columns, types }) => {
   let min;
   let max;
 
-  for (const [, Y] of data) {
-    if (typeof min !== 'number') min = Y;
-    if (typeof max !== 'number') max = Y;
+  columns.map(col => {
+    if (types[col[0]] === 'line') {
+      if (typeof min !== 'number') min = col[1];
+      if (typeof max !== 'number') max = col[1];
 
-    if (min > Y) min = Y;
-    if (max < Y) max = Y;
-  }
+      if (min > col[1]) min = col[1];
+      if (max < col[1]) max = col[1];
+      
+      for (let i = 2; i < col.length; i++) {
+        if (min > col[i]) min = col[i];
+        if (max < col[i]) max = col[i];
+      }
+    }
+  });
 
   return {
     MIN: min,
@@ -47,7 +60,7 @@ const getMinMax = data => {
 // Draw Y axis lines
 const drawGrid = (ctx, data) => {
   const { MIN, MAX } = getMinMax(data);
-  const STEP = VIEW_HEIGHT / GRID_LINES; // step between grid lines 
+  const STEP = VIEW.HEIGHT / GRID_LINES; // step between grid lines 
   const LABEL_STEP = (MAX - MIN) / GRID_LINES; // step between labels values
 
   ctx.beginPath();
@@ -58,8 +71,8 @@ const drawGrid = (ctx, data) => {
 
   for (let i = 1; i <= GRID_LINES; i++) {
     const Y = STEP * i;
-    const Y_CHART = Y + PADDING;
-    const LABEL = MAX - LABEL_STEP * i;
+    const Y_CHART = Y + PADDING; // y coordinate for grid line
+    const LABEL = Math.round(MAX - LABEL_STEP * i); // label text
 
     // Display grid labels
     ctx.fillText(LABEL, 5, Y_CHART - 10);
@@ -72,11 +85,26 @@ const drawGrid = (ctx, data) => {
   ctx.closePath();
 }
 
-// Initialize canvas
+// Draw chart line
+const drawChartLine = (ctx, coords, {color}) => {
+  ctx.beginPath();
+  ctx.lineWidth = THEME.LINE_WIDTH;
+  ctx.strokeStyle = color;
+
+  coords.map(([x, y]) => {
+    ctx.lineTo(x, y);
+  });
+
+  ctx.stroke();
+  ctx.closePath();
+};
+
+// Initialize canvas chart
 const chart = (canvas, data) => {
   const ctx = canvas.getContext('2d');
   const { MIN, MAX } = getMinMax(data);
-  const RATIO = VIEW_HEIGHT / (MAX - MIN); // chart scale ratio
+  const RATIO_Y = VIEW.HEIGHT / (MAX - MIN); // chart scale ratio by y axis
+  const RATIO_X = VIEW.WIDTH / (data.columns[0].length - 2); // chart scale ratio by x axis
   
   // Set canvas element size
   canvas.style.width = SIZES.WIDTH + 'px';
@@ -87,26 +115,24 @@ const chart = (canvas, data) => {
   canvas.height = DPI_SIZES.HEIGHT;
 
   drawGrid(ctx, data);
-  
-  // Draw chart line
-  ctx.beginPath();
-  ctx.lineWidth = THEME.LINE_PRIMARY.WIDTH;
-  ctx.strokeStyle = THEME.LINE_PRIMARY.COLOR;
 
-  data.map(([x, y]) => {
-    ctx.lineTo(x, DPI_SIZES.HEIGHT - y * RATIO - PADDING);
+  data.columns.map(col => {
+    const name = col[0]; // column name
+    // Draw only lines
+    if (data.types[name] === 'line') {
+      // Point coodinates
+      const coords = col.map((y, i) => [
+        Math.round((i - 1) * RATIO_X),
+        Math.round(DPI_SIZES.HEIGHT - y * RATIO_Y - PADDING)
+      ]).filter((_, i) => i !== 0);
+
+      drawChartLine(ctx, coords, {
+        color: data.colors[name]
+      });
+    }
   });
-
-  ctx.stroke();
-  ctx.closePath();
 };
 
-chart(document.querySelector('#chart'), [
-  [0, 0],
-  [200, 250],
-  [400, 320],
-  [600, 270],
-  [800, 300],
-  [1000, 380],
-  [1200, 350],
-]);
+getData('data.json').then(data => {
+  chart(document.querySelector('#chart'), data);
+});
