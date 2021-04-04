d0474d21153645462c6bcc44963e9384e31d48e0
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-04-04 15:04:01 +0300

    Draw position line and point

--- chart.js
+++ chart.js
@@ -1,6 +1,6 @@
 // Constants
 const SIZES = {
-  WIDTH: 600,
+  WIDTH: 800,
   HEIGHT: 200
 };
 const DPI_SIZES = {
@@ -19,11 +19,18 @@ const THEME = {
     WIDTH: 1,
     COLOR: '#CCCCCC'
   },
+	POINT_LINE: {
+		WIDTH: 2,
+		COLOR: '#DDDDDD'	
+	},
   TICKS: {
     FONT: 'normal 20px Helvetica, sans-serif',
     FILL: '#96A2AA'
   },
-  LINE_WIDTH: 4
+  LINE_WIDTH: 4,
+	POINT: {
+		RADIUS: 6
+	}
 };
 
 // Fetch data from JSON file
@@ -88,18 +95,36 @@ const drawGrid = (ctx, { MIN, MAX }) => {
   ctx.closePath();
 }
 
-// Draw ticks labels for X axis
-const drawXTicks = (ctx, data, RATIO_X) => {
-  const STEP = Math.round(data.length / TICKS_AMOUNT); // step between ticks
+// Draw elements for X axis (labels and position line)
+const drawXElements = (ctx, data, RATIO_X, { mouse }) => {
+  const STEP = Math.round(data.length / TICKS_AMOUNT); // step between labels
 
   ctx.beginPath();
+	ctx.lineWidth = THEME.POINT_LINE.WIDTH;
+	ctx.strokeStyle = THEME.POINT_LINE.COLOR;
   
-  for (let i = 1; i < data.length; i += STEP) {
-    const LABEL = getDate(new Date(data[i]));
-  
-    ctx.fillText(LABEL, i * RATIO_X, DPI_SIZES.HEIGHT - 10);
+  for (let i = 1; i < data.length; i++) {
+		const X = i * RATIO_X; // scaled X axis coordinate
+
+		// Draw labels
+		if ((i - 1) % STEP === 0) {
+			const LABEL = getDate(new Date(data[i])); // label text
+		
+			ctx.fillText(LABEL, X, DPI_SIZES.HEIGHT - 10);
+		}
+
+		// Draw position line
+		if (isOver(mouse, X, data.length)) {
+			// Save context state
+			ctx.save();
+			ctx.moveTo(X, PADDING / 2);
+			ctx.lineTo(X, DPI_SIZES.HEIGHT - PADDING);
+			// Load context state
+			ctx.restore();
+		}
   }
 
+	ctx.stroke();
   ctx.closePath();
 };
 
@@ -117,20 +142,95 @@ const drawChartLine = (ctx, coords, {color}) => {
   ctx.closePath();
 };
 
+// Draw position point
+const drawChartPoint = (ctx, color, { x, y }) => {
+	ctx.beginPath();
+	ctx.strokeStyle = color;
+	ctx.fillStyle = '#FFFFFF';
+	ctx.arc(x, y, THEME.POINT.RADIUS, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.stroke();
+	ctx.closePath();
+};
+
 // Get coordinates from data for drawing lines
 const getCoordinates = (RATIO_X, RATIO_Y) => col => col.map((y, i) => [
   Math.round((i - 1) * RATIO_X),
   Math.round(DPI_SIZES.HEIGHT - y * RATIO_Y - PADDING)
 ]).filter((_, i) => i !== 0);
 
+// Check whether is mouse over coordinate
+const isOver = (mouse, x, length) => {
+	if (mouse) {
+		const STEP = DPI_SIZES.WIDTH / length; // step between two X axis values
+
+		// Return true if mouse is closer to x than to sibling value
+		return Math.abs(x - mouse.x) < STEP / 2;
+	}
+};
+
 // Initialize canvas chart
 const chart = (canvas, data) => {
+	// Draw position over chart elements
+	const mouseMoveHandler = ({ clientX }) => {
+		const { left } = canvas.getBoundingClientRect(); // canvas position relatively to the window
+
+		// Set mouse X position relatively to canvas
+		proxy.mouse = {
+			x: (clientX - left) * 2
+		};
+	};
+
+	// Remove position over chart elements (remove mouse X position)
+	const mouseLeaveHandler = () => {
+		proxy.mouse = null;
+	};
+
+	// Draw whole chart
+	const draw = () => {
+		erase();
+
+		drawGrid(ctx, {MIN, MAX});
+		drawXElements(ctx, X_DATA.filter((_, i) => i !== 0), RATIO_X, proxy);
+	
+		// Draw chart lines by coordinates
+		LINES_DATA.map(getCoordinates(RATIO_X, RATIO_Y)).map((coords, i) => {
+			const COLOR = data.colors[LINES_DATA[i][0]]; // current chart line and point color
+
+			drawChartLine(ctx, coords, {
+				color: COLOR
+			});
+
+			// Draw chart point if mouse is over coordinate
+			coords.map(([x, y]) => {
+				if (isOver(proxy.mouse, x, coords.length)) {
+					drawChartPoint(ctx, COLOR, {x, y});
+				}
+			});
+		});
+	};
+
+	// Clear whole canvas
+	const erase = () => {
+		ctx.clearRect(0, 0, DPI_SIZES.WIDTH, DPI_SIZES.HEIGHT);
+	};
+
+	let requestId;
   const ctx = canvas.getContext('2d');
   const { MIN, MAX } = getMinMax(data);
   const RATIO_Y = VIEW.HEIGHT / (MAX - MIN); // chart scale ratio by y axis
   const RATIO_X = VIEW.WIDTH / (data.columns[0].length - 2); // chart scale ratio by x axis
   const LINES_DATA = data.columns.filter(col => data.types[col[0]] === 'line'); // data to draw chart lines
   const X_DATA = data.columns.filter(col => data.types[col[0]] === 'x')[0]; // data to draw x ticks
+	const proxy = new Proxy({}, {
+		set(...args) {
+			// Redraw whole chart
+			requestId = requestAnimationFrame(draw, canvas);
+
+			// Write values to object and return true if property was changed
+			return Reflect.set(...args);
+		}
+	}); // proxying changes over mouse object
   
   // Set canvas element size
   canvas.style.width = SIZES.WIDTH + 'px';
@@ -140,18 +240,29 @@ const chart = (canvas, data) => {
   canvas.width = DPI_SIZES.WIDTH;
   canvas.height = DPI_SIZES.HEIGHT;
 
-  drawGrid(ctx, {MIN, MAX});
-  drawXTicks(ctx, X_DATA.filter((_, i) => i !== 0), RATIO_X);
+	// Add handlers to mouse events
+	canvas.addEventListener('mousemove', mouseMoveHandler);
+	canvas.addEventListener('mouseleave', mouseLeaveHandler);
 
-	// Draw chart lines by coordinates
-  LINES_DATA.map(getCoordinates(RATIO_X, RATIO_Y)).map((coords, i) => {
-    drawChartLine(ctx, coords, {
-      color: data.colors[LINES_DATA[i][0]]
-    });
-  });
+	return {
+		// Initialize chart method
+		init() {
+			draw();
+		},
+		// Destroy chart method
+		destroy() {
+			// Cancel request for animation frame
+			cancelAnimationFrame(requestId);
+			// Remove handlers of mouse events
+			canvas.removeEventListener('mousemove', mouseMoveHandler);
+			canvas.removeEventListener('mouseleave', mouseLeaveHandler);
+		}
+	};
 };
 
 // Execute drawing chart
 getData('data.json').then(data => {
-  chart(document.querySelector('#chart'), data);
+  const chartElement = chart(document.querySelector('#chart'), data);
+
+	chartElement.init();
 });
